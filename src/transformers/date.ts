b291fd1c2c67c368c8d027c1c8a9cf8dbb8e06a8
Rokoucha <git@rokoucha.net>
2020-09-17 02:35:48 +0900

    feat: dateString

--- src/transformers/date.ts
+++ src/transformers/date.ts
@@ -11,3 +11,15 @@ export const $unixtime = Transformer.from<number, Date>(time => {
   const date = new Date(time * 1000)
   return isNaN(date.getTime()) ? error(ValidationError.from(new InvalidUnixtimeError(time))) : ok(date)
 })
+
+export class InvalidDateStringError extends Error {
+  constructor(readonly date: string) {
+    super(`"${date}" is invalid unixtime.`)
+    this.name = 'InvalidDateStringError'
+  }
+}
+
+export const $dateString = Transformer.from<string, Date>(text => {
+  const date = new Date(text)
+  return isNaN(date.getTime()) ? error(ValidationError.from(new InvalidDateStringError(text))) : ok(date)
+})
