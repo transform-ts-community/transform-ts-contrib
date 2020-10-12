e2a36e306162b7fc80b56699d029f15dd1fb12c2
Rokoucha <git@rokoucha.net>
2020-10-12 22:41:41 +0900

    docs: JSDoc で ISO 8601 を強く推奨

--- src/transformers/date.ts
+++ src/transformers/date.ts
@@ -7,6 +7,11 @@ export class InvalidUnixtimeError extends Error {
   }
 }
 
+/**
+ * Convert unixtime string to Date object
+ *
+ * @param time Converted as seconds instead of milliseconds
+ */
 export const $unixtime = Transformer.from<number, Date>(time => {
   const date = new Date(time * 1000)
   return isNaN(date.getTime()) ? error(ValidationError.from(new InvalidUnixtimeError(time))) : ok(date)
@@ -19,6 +24,11 @@ export class InvalidDateStringError extends Error {
   }
 }
 
+/**
+ * Convert string to Date object
+ *
+ * @param text Strongly recommend to use ISO 8601 format
+ */
 export const $dateString = Transformer.from<string, Date>(text => {
   const date = new Date(text)
   return isNaN(date.getTime()) ? error(ValidationError.from(new InvalidDateStringError(text))) : ok(date)
